84db5f99a54d865698bbbbd2ad977f8140ff95e7
Henry <Henries90@gmail.com>
2025-09-01 22:17:44 +0100

    Added URL to cors options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,7 @@
 // config/corsOptions.js
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://homedoc-backend-69l2.onrender.com"
   // "http://localhost:3000","https://your-production-domain.com",
 ];
 
